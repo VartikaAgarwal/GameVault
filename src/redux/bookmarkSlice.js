df1d05198b47b48bc9453246a051339bdfa7878f
VartikaAgarwal <124383384+VartikaAgarwal@users.noreply.github.com>
2025-04-05 22:24:10 +0530

    Update bookmarkSlice.js

--- src/redux/bookmarkSlice.js
+++ src/redux/bookmarkSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favorites: [], // Array of bookmarked games
+  favorites: [], 
 };
 
 const bookmarkSlice = createSlice({
